e0ef92c3a2d2a59ec643a059f4f158a1d13ce64c
Mostafa Maki <mostafa_m9@yahoo.com>
2022-12-24 14:31:32 -0800

    set up socket io on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    setSocket(io('http://localhost:4000'))
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <form>
+        <input></input>
+        <button>Add</button>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:3000']
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    // console.log('A user connected!');
+    console.log('A user connected!');
     socket.on('chat message', (msg) => {
         console.log(`message: ${msg}`)
         io.emit('chat message', msg)
@@ -20,6 +24,6 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(3000, () => {
-    console.log('Listening on *:3000')
+server.listen(4000, () => {
+    console.log('Listening on *:4000')
 });
