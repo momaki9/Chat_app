14d5486258dd9a8d27c871522ae07679e6a4603d
momaki9 <mostafa_m9@yahoo.com>
2022-12-31 18:37:07 -0800

    implemented login feature on client side

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 import {
     FormControl,
     FormLabel,
@@ -15,9 +18,42 @@ import {
 } from "@chakra-ui/react";
 
 function Login() {
+
     const { isOpen, onOpen, onClose } = useDisclosure();
     const initialRef = React.useRef(null);
     const finalRef = React.useRef(null);
+
+    const [formState, setFormState] = useState({
+        username: '',
+        password: ''
+    });
+    const [login, { error, data }] = useMutation(LOGIN_USER);
+
+    const handleFormChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await login({
+                variables: { ...formState }
+            });
+            Auth.login(data.login.token)
+
+        } catch (err) {
+            console.error(err)
+        }
+        setFormState({
+            username: '',
+            password: ''
+        })
+    }
+
     return (
         <>
             <Button onClick={onOpen}>Login</Button>
@@ -28,26 +64,41 @@ function Login() {
                 onClose={onClose}
             >
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Login</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <FormLabel>Username</FormLabel>
-                            <Input ref={initialRef} placeholder='Username' />
-                        </FormControl>
-                        <FormControl mt={4}>
-                            <FormLabel>Password</FormLabel>
-                            <Input placeholder='Password' />
-                        </FormControl>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3}>
-                            Login
-                        </Button>
-                        <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
+                <form onSubmit={handleLogin}>
+                    <ModalContent>
+                        <ModalHeader>Login</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={6}>
+                            <FormControl>
+                                <FormLabel>Username</FormLabel>
+                                <Input
+                                    ref={initialRef}
+                                    placeholder='Username'
+                                    name="username"
+                                    type={"text"}
+                                    value={formState.username}
+                                    onChange={handleFormChange}
+                                />
+                            </FormControl>
+                            <FormControl mt={4}>
+                                <FormLabel>Password</FormLabel>
+                                <Input
+                                    placeholder='Password'
+                                    name="password"
+                                    type={"password"}
+                                    value={formState.password}
+                                    onChange={handleFormChange}
+                                />
+                            </FormControl>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3} type='submit'>
+                                Login
+                            </Button>
+                            <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </form>
             </Modal>
         </>
     )
